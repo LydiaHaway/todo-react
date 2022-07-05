4768f49b72f29773d984cc87ad60773ab6850837
Lydia <lydia.haway@gmail.com>
2022-07-05 14:54:21 +0200

    ex

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  return <TodoList />;
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  let handleAddTodo = (e) => {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 
 export default App;
